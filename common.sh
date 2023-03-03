96c1af525981237de5640cb325365616725a5f12
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-03 20:01:35 +0530

    created the log file using func and vars

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 code_dir=${pwd}
-log_file=/tmp/roboshop.1og
+log_file=/tmp/roboshop.log #creating a log file. the log gets updated by using redirectors.
 rm -f ${1og_file}
 print_head() {
    echo -е "\e[36m$1\e[0m"
