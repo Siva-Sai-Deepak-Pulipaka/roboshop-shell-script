3efe9ae7e893705ad912f22f809ceb1c5f061a98
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-04 01:27:49 +0530

    updated listen address line in mongo

--- mongodb.sh
+++ mongodb.sh
@@ -14,6 +14,6 @@ systemctl enable mongod &>>${log_file}
 systemctl start mongod &>>${log_file}
 #we need to update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf so we are using sed command
 print_head "updating listen address from 127.0.0.1 to 0.0.0.0"
-sed -i -e "s/127.0.0.1/0.0.0.0/g" &>>${log_file}
+sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>>${log_file}
 print_head "restarting mongodb server"
 systemctl restart mongod &>>${log_file}
