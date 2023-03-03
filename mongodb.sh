dcca352518efa12273116026e70ee28742475831
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-03 17:21:27 +0530

    added colours and printing

--- mongodb.sh
+++ mongodb.sh
@@ -3,4 +3,5 @@ yum install mongodb-org -y
 systemctl enable mongod 
 systemctl start mongod 
 #we need to update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf so we are using sed command
+sed -i -e "s/127.0.0.1/0.0.0.0/g"
 systemctl restart mongod
