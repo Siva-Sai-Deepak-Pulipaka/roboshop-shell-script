349784e73ddc8a86e5f6c57d1653fb5c05ccd7e1
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-03 19:39:03 +0530

    commands in variables can be declared with ()

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,29 @@
 #source common.sh
-#dir=${pwd}
+dir=$(pwd)
 print_head()
 {
-    echo -e "\e[36m$1\e[0m"
+    echo -e "\e[32m$1\e[0m"
 }
 print_head "installing nginx"
 yum install nginx -y 
+
 print_head "removing old content in nginx html"
 rm -rf /usr/share/nginx/html/* 
+
 print_head "downloading frontend component"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+
 #cp config-files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 #The above line is the actual command to access the config file. But after we run the above commands based on document we will be in /usr/share/nginx/html so config-files directory is not presented here. so we are declaring variable at the start. Because script always run from top to bottom.
+
 print_head "copying nginx configuration file "
-cp config-files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${dir}/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 print_head "enabling nginx"
 systemctl enable nginx 
+
 #we have implemented restart because nginx gets updated everytime we make changes in our components
+
 print_head "restarting nginx"
 systemctl restart nginx 
