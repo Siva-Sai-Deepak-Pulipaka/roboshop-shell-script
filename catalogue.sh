65151a2d5647a11edc75a57af7bc5f65b539f5a4
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-04 01:12:22 +0530

    appended log file

--- catalogue.sh
+++ catalogue.sh
@@ -6,36 +6,36 @@ print_head()
 }
 print_head "running the script to get nodejs repo file"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+yum install nodejs -y &>>${log_file}
 
 print_head "adding roboshop-user"
-useradd roboshop
+useradd roboshop &>>${log_file}
 
 echo -e "\e[m34Creating app directory\e[0m" # just testing the echo command 
-mkdir /app 
+mkdir /app &>>${log_file}
 rm -rf /app/* #we are removing the contents to sattisfy code standard (re-run should never fail)
 
 print_head "downloading the catalogue folder"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app &>>${log_file}
 
 print_head "extracted files"
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>${log_file}
 
 print_head "installing node package manager"
 npm install 
 
 print_head "creating catalogue service file"
-cp config-files/catalogue.service /etc/systemd/system/catalogue.service
+cp config-files/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
 print_head "changes updated, enabled and started catalogue server"
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+systemctl daemon-reload &>>${log_file}
+systemctl enable catalogue &>>${log_file}
+systemctl start catalogue &>>${log_file}
 
 print_head "installing mongodb repo"
-cp config-files/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp config-files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "installing mongo client and connected to host"
-yum install mongodb-org-shell -y
-mongo --host mongodb.easydevops.online </app/schema/catalogue.js
+yum install mongodb-org-shell -y &>>${log_file}
+mongo --host mongodb.easydevops.online </app/schema/catalogue.js &>>${log_file}
