ee673d5ad1d77bb9a4489b323f2e6647c8e4c731
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-03 20:20:33 +0530

    updating common file

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
-#source common.sh
+#source common.sh,
 code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file} #we want to get updated log for every command so we are using this strategy that we are removing log file earlier.
