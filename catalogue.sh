3591474bd13c00382adfa8075aa52c3e5e097223
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-05 20:57:23 +0530

    merging user and catalogue by NODEJS fun in common

--- catalogue.sh
+++ catalogue.sh
@@ -1,36 +1,4 @@
 source common.sh
-print_head "running the script to get nodejs repo file"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-yum install nodejs -y &>>${log_file}
-
-print_head "adding roboshop-user"
-useradd roboshop &>>${log_file}
-
-echo -e "\e[34mCreating app directory\e[0m"  # just testing the echo command 
-mkdir /app &>>${log_file}
-rm -rf /app/* #we are removing the contents to sattisfy code standard (re-run should never fail)
-
-print_head "downloading the catalogue folder"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-cd /app &>>${log_file}
-
-print_head "extracted files"
-unzip /tmp/catalogue.zip &>>${log_file}
-
-print_head "installing node package manager"
-npm install &>>${log_file}
-
-print_head "creating catalogue service file"
-cp ${code_dir}/config-files/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
-
-print_head "changes updated, enabled and started catalogue server"
-systemctl daemon-reload &>>${log_file}
-systemctl enable catalogue &>>${log_file}
-systemctl start catalogue &>>${log_file}
-
-print_head "installing mongodb repo"
-cp ${code_dir}/config-files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-
-print_head "installing mongo client and connected to host"
-yum install mongodb-org-shell -y 
-mongo --host mongodb.easydevops.online </app/schema/catalogue.js 
+#for both user and user steps are same so to follow code standards we are making it as NODEJS function in common.sh and just changing component names using a variable component.
+component=user
+NODEJS
--- user.sh
+++ user.sh
@@ -1,43 +1,5 @@
 source common.sh
-user(){
 
-print_head "installing node repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-
-print_head "installing nodejs"
-yum install nodejs -y &>>${log_file}
-
-print_head "creating roboshop user"
-useradd roboshop &>>${log_file}
-
-print_head "creating app directory"
-mkdir /app &>>${log_file}
-
-print_head "removing contents if any"
-rm -rf /app/*
-
-print_head "Downloading zip file and switched to app directory"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log_file} 
-cd /app &>>${log_file}
-
-print_head "extract zip file"
-unzip /tmp/user.zip &>>${log_file}
-
-print_head "installing node package manager"
-npm install &>>${log_file}
-
-print_head "copying service file"
-cp ${code_dir}/config-files/user.service /etc/systemd/system/user.service &>>${log_file}
-systemctl daemon-reload &>>${log_file}
-systemctl enable user 
-systemctl start user
-
-print_head "installing mongodb repo"
-cp ${code_dir}/config-files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-
-print_head "installing mongo client and connected to host"
-yum install mongodb-org-shell -y &>>${log_file}
-mongo --host mongodb.easydevops.online </app/schema/user.js &>>${log_file}
-
-}
-user
+    #for both user and user steps are same so to follow code standards we are making it as NODEJS function in common.sh and just changing component names using a variable component.
+component=user
+NODEJS
