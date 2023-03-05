898cd94769b4bde18b418d8294e319c98e2f5921
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-05 20:58:15 +0530

    variable changed to catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,4 @@
 source common.sh
 #for both user and user steps are same so to follow code standards we are making it as NODEJS function in common.sh and just changing component names using a variable component.
-component=user
+component=catalogue
 NODEJS
