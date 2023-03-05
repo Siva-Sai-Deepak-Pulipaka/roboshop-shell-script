7f9af206a23cdf52ac28317d1ac35c8481b4eb7f
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-05 18:20:59 +0530

    just to see error

--- redis.sh
+++ redis.sh
@@ -2,13 +2,13 @@ source common.sh
 echo -e "\e[33mInstalling repo\e[0m"
 status_check $?
 
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>{log_file}
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y 
 status_check $?
-dnf module enable redis:remi-6.2 -y &>>{log_file}       
+dnf module enable redis:remi-6.2 -y        
 status_check $?
-yum install redis -y &>>{log_file}
+yum install redis -y 
 status_check $?
-sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf &>>{log_file}
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf 
 status_check $?
-systemctl enable redis &>>{log_file}
-systemctl start redis &>>{log_file}
+systemctl enable redis 
+systemctl start redis 
