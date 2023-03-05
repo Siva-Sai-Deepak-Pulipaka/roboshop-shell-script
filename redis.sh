5fedcbbc88fd50ccf466726249b00d2ba88cf871
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-05 19:37:11 +0530

    append logs, color to user  and logs to  redis

--- redis.sh
+++ redis.sh
@@ -12,6 +12,7 @@ status_check $?
 print_head "installing redis 6.2"
 yum install redis -y &>>${log_file}
 status_check $?
+
 print_head "updating listen address from 127.0.0.1 to 0.0.0.0"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
 status_check $?
--- user.sh
+++ user.sh
@@ -1,18 +1,35 @@
+source common.sh
 user(){
 
 code_dir=$(pwd)
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+print_head "installing node repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-yum install nodejs -y
-useradd roboshop
-mkdir /app
+print_head "installing nodejs"
+yum install nodejs -y &>>${log_file}
+
+print_head "creating roboshop user"
+useradd roboshop &>>${log_file}
+
+print_head "creating app directory"
+mkdir /app &>>${log_file}
+
+print_head "removing contents if any"
 rm -rf /app/*
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
-cd /app 
-unzip /tmp/user.zip
-npm install 
-cp ${code_dir}/config-files/user.service /etc/systemd/system/user.service
-systemctl daemon-reload
+
+print_head "Downloading zip file and switched to app directory"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log_file} 
+cd /app &>>${log_file}
+
+print_head "extract zip file"
+unzip /tmp/user.zip &>>${log_file}
+
+print_head "installing node package manager"
+npm install &>>${log_file}
+
+print_head "copying service file"
+cp ${code_dir}/config-files/user.service /etc/systemd/system/user.service &>>${log_file}
+systemctl daemon-reload &>>${log_file}
 systemctl enable user 
 systemctl start user
 
