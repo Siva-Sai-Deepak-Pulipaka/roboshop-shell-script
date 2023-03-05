14a78af76cec2e90f53d95e5afa28bd28ec5cb91
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-05 18:18:19 +0530

    changes done

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ dnf module enable redis:remi-6.2 -y &>>{log_file}
 status_check $?
 yum install redis -y &>>{log_file}
 status_check $?
-sed -i -e "s/127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>{log_file}
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf &>>{log_file}
 status_check $?
 systemctl enable redis &>>{log_file}
 systemctl start redis &>>{log_file}
