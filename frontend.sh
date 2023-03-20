f5fb0a91aba6986bc59a43c207175df15e0e5567
Siva-Sai-Deepak-Pulipaka <18l11a0518@gmail.com>
2023-03-20 13:13:47 +0530

    updated with status checking function

--- frontend.sh
+++ frontend.sh
@@ -2,16 +2,19 @@ source common.sh
 
 print_head "installing nginx"
 yum install nginx -y &>>${log_file}
+status_check $?
 
 #&>> means appending output and also errors to a single file named log_file
 
 print_head "removing old content in nginx html"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
 
 print_head "downloading frontend component"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 #cp config-files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 #The above line is the actual command to access the config file. But after we run the above commands based on document we will be in /usr/share/nginx/html so config-files directory is not presented here. so we are declaring variable at the start. Because script always run from top to bottom.
@@ -20,8 +23,10 @@ print_head "copying nginx configuration file "
 cp ${code_dir}/config-files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 #we have implemented restart because nginx gets updated everytime we make changes in our components
 
 print_head "restarting nginx"
 systemctl restart nginx &>>${log_file}
+status_check $?
